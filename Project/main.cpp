6a13a8d959ce8e20476c86e36ec1468076bcc701
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-03 11:09:40 +0200

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,21 +1,28 @@
 #include "Engine.h"
 
-int main()
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+int main(int, char* [])
 {
 	// DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1 = 1
 	//DISABLE_LAYER_NV_OPTIMUS_1 = 1
 	//_putenv_s("DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1", "1");
 	//_putenv_s("DISABLE_LAYER_NV_OPTIMUS_1", "1");
 
-	try
-	{
-		Engine app;
-		app.Run();
-	}
-	catch (const std::exception& e) 
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
+	Engine app;
+	app.Run();
+
+	//try
+	//{
+	//	Engine app;
+	//	app.Run();
+	//}
+	//catch (const std::exception& e) 
+	//{
+	//	std::cerr << e.what() << std::endl;
+	//	return EXIT_FAILURE;
+	//}
+
 	return EXIT_SUCCESS;
 }
