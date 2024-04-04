bf7488fc8140c10d64a923abf372d455100b2592
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-04 13:37:17 +0200

    Updated Material class
    Material is defined by a vertex type and a pipeline type. The material only holds a pipeline and can be set (non)-/active

--- Project/Material/Material.h
+++ Project/Material/Material.h
@@ -2,49 +2,76 @@
 #define MATERIAL_H
 
 #include "Pipelines/Pipeline.h"
-#include "Core/SwapChain.h"
+#include "Core/CommandPool.h"
+#include "Util/Concepts.h"
+#include "Mesh/Mesh.h"
+#include "BaseMaterial.h"
 
-#include "Util/Logger.h"
-
-class BaseMesh;
 class SwapChain;
 
-class Material final
+template <pipeline_type P, vertex_type V>
+class Material final : public BaseMaterial
 {
 public:
-	explicit Material() = default;
-	~Material() = default;
+	explicit Material(const VulkanContext& context)
+	{
+		m_pPipeline = std::make_unique<P>();
+		m_pPipeline->CreatePipeline(context);
+	}
+
+	virtual ~Material() override = default;
 
 	Material(const Material&) = delete;
-	Material operator=(const Material&) = delete;
+	Material& operator=(const Material&) = delete;
 	Material(Material&&) = delete;
-	Material operator=(Material&&) = delete;
+	Material& operator=(Material&&) = delete;
 
-	void CleanUp(const GameContext& context);
+	virtual void CleanUp(const GameContext& context) override
+	{
+		m_pPipeline->CleanUp(context.vulkanContext.device);
+	}
 
-	template <typename tPipeline>
-		requires std::is_base_of_v<Pipeline, tPipeline>
-	void AddPipeline(const GameContext& context);
-
-	void BindMesh(BaseMesh* pMesh);
+	void BindMesh(const GameContext& context, Mesh<V>* pMesh)
+	{
+		m_pMeshes.push_back(pMesh);
+		if (auto layout = m_pPipeline->GetDescriptorSetLayout();
+			layout != VK_NULL_HANDLE)
+			m_pMeshes.back()->CreateDescriptor(context, layout);
+	}
 
 	Pipeline* GetPipeline() const { return m_pPipeline.get(); }
-	void DrawFrame(const GameContext& context) const;
 
-	bool IsActive() const { return m_IsActive; }
-	void SetIsActive(bool isActive) { m_IsActive = isActive; }
+	virtual void DrawFrame(uint32_t currentFrame, VkExtent2D extent) const override
+	{
+		const auto commandBuffer = CommandPool::GetInstance().GetCommandBuffer(currentFrame);
+
+		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pPipeline->GetPipeline());
+
+		VkViewport viewport{};
+		viewport.x = 0.0f;
+		viewport.y = 0.0f;
+		viewport.width = (float)extent.width;
+		viewport.height = (float)extent.height;
+		viewport.minDepth = 0.0f;
+		viewport.maxDepth = 1.0f;
+		vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+		VkRect2D scissor{};
+		scissor.offset = { 0, 0 };
+		scissor.extent = extent;
+		vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+		// DrawScene();
+		for (const auto& m : m_pMeshes)
+		{
+			m->Update(currentFrame, commandBuffer, m_pPipeline->GetPipelineLayout());
+			m->Draw(commandBuffer);
+		}
+	}
 
 private:
-	bool m_IsActive{ true };
 	std::unique_ptr<Pipeline> m_pPipeline{ nullptr };
-	std::vector<BaseMesh*> m_pMeshes;
+	std::vector<Mesh<V>*> m_pMeshes;
 };
 
-template <typename tPipeline>
-	requires std::is_base_of_v<Pipeline, tPipeline>
-void Material::AddPipeline(const GameContext& context)
-{
-	m_pPipeline = std::make_unique<tPipeline>();
-	m_pPipeline->CreatePipeline(context.vulkanContext);
-}
 #endif // MATERIAL_H
