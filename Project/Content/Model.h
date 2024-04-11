448ccb441ac3ea136f1ea9ee12416a5a6edd9904
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-11 14:46:08 +0200

    Wrapper class for a model loaded from an obj file

---
+++ Project/Content/Model.h
@@ -0,0 +1,30 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "Util/Structs.h"
+
+// TODO: Finish tutorial
+class Model final
+{
+public:
+	explicit Model(const std::string& path, const glm::vec3& pos);
+	~Model() = default;
+
+	Model(const Model&) = delete;
+	Model& operator=(const Model&) = delete;
+	Model(Model&&) = delete;
+	Model& operator=(Model&&) = delete;
+
+	//void CleanUp(const GameContext& context) const;
+
+	std::vector<PosTexNorm> GetVertices() const { return m_Vertices; }
+	std::vector<uint32_t> GetIndices() const { return m_Indices; }
+
+private:
+	std::vector<PosTexNorm> m_Vertices;
+	std::vector<uint32_t> m_Indices;
+
+	void Load(const std::string& path, glm::vec3);
+};
+
+#endif // MODEL_H
