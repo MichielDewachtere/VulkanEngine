8118125d016025cfc274fb38b85661f2498aff89
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-11 14:45:48 +0200

    Added contentmanager
    This manager loads and manages everything content related, for now that would be textures and models

---
+++ Project/Content/ContentManager.h
@@ -0,0 +1,46 @@
+#ifndef CONTENTMANAGER_H
+#define CONTENTMANAGER_H
+
+#include <map>
+#include <memory>
+
+#include "Model.h"
+#include "Texture2D.h"
+
+#include "Util/Structs.h"
+#include "Util/Singleton.h"
+
+class ContentManager final : public Singleton<ContentManager> 
+{
+public:
+	virtual ~ContentManager() override = default;
+
+	Model* LoadModel(const std::string& path, const glm::vec3& pos);
+	Texture2D* LoadTexture(const GameContext& context, const std::string& path);
+
+	void CleanUp(const GameContext& context);
+
+private:
+	friend class Singleton<ContentManager>;
+	ContentManager() = default;
+
+	std::map<std::string, std::unique_ptr<Texture2D>> m_Images;
+	std::map<std::string, std::unique_ptr<Model>> m_Models;
+
+	//template<typename T, typename... Args>
+	//T* LoadContent(std::map<std::string, std::unique_ptr<T>>&, const std::string& path, Args... args);
+};
+
+//template <typename T, typename ... Args>
+//T* ContentManager::LoadContent(std::map<std::string, std::unique_ptr<T>>& map, const std::string& path, Args... args)
+//{
+//	if (map.contains(path))
+//	{
+//		return map.at(path).get();
+//	}
+//
+//	map.emplace(path, std::make_unique<T>(path, args));
+//	return map.at(path).get();
+//}
+
+#endif // CONTENTMANAGER_H
