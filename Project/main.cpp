fdec132347f88bb37012b0a8fe983e1d05765a70
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-07 11:26:39 +0200

    Updated everything to work with RealCore

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,6 +3,27 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
+#include <real_core/SceneManager.h>
+
+#include "Scenes/TestScene.h"
+#include "Material/MaterialManager.h"
+#include "Material/Pipelines/PosCol2DPipeline.h"
+#include "Material/Pipelines/PosColNormPipeline.h"
+#include "Material/Pipelines/PosTexNormPipeline.h"
+
+void Load(const GameContext& context)
+{
+	auto& materialManager = MaterialManager::GetInstance();
+	materialManager.AddMaterial<PosCol2DPipeline, PosCol2D>(context);
+	materialManager.AddMaterial<PosColNormPipeline, PosColNorm>(context);
+	materialManager.AddMaterial<PosTexNormPipeline, PosTexNorm>(context);
+
+	auto& sceneManager = real::SceneManager::GetInstance();
+
+	sceneManager.CreateScene(new TestScene("test scene", "test input map"));
+	sceneManager.SetSceneActive("test scene");
+}
+
 int main(int, char* [])
 {
 	// DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1 = 1
@@ -10,13 +31,10 @@ int main(int, char* [])
 	//_putenv_s("DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1", "1");
 	//_putenv_s("DISABLE_LAYER_NV_OPTIMUS_1", "1");
 
-	//RealEngine app;
-	//app.Run();
-
 	try
 	{
 		RealEngine app;
-		app.Run();
+		app.Run(Load);
 	}
 	catch (const std::exception& e) 
 	{
