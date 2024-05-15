23c6c7e757e6e5d9ebdfff0040bdf0ce08492551
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-15 18:56:29 +0200

    Updated Camera to work with RealCore

--- Real3D/Misc/Camera.cpp
+++ Real3D/Misc/Camera.cpp
@@ -1,13 +1,12 @@
 // ReSharper disable CppRedundantQualifier
 #include "Camera.h"
 
-#include <real_core/GameTime.h>
 #include <real_core/GameObject.h>
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "CameraInputManager.h"
+//#include "CameraInputManager.h"
 #include "RealEngine.h"
 #include "Graphics/Renderer.h"
 #include "Core/SwapChain.h"
@@ -21,22 +20,6 @@ real::Camera::Camera(real::GameObject* pOwner)
 void real::Camera::Update()
 {
 	auto context = RealEngine::GetGameContext();
-
-	// Handle Input
-	const auto elapsedTime = real::GameTime::GetInstance().GetElapsed();
-	HandleKeyboardInput(elapsedTime);
-	HandleMouseInput(elapsedTime);
-
-	// Calculate rotation quaternion
-	const auto rot = glm::quat(glm::vec3(glm::radians<float>(m_Pitch), glm::radians<float>(m_Yaw), 0.0f));
-
-	// Create rotation matrix
-	const auto rotMat = glm::mat4_cast(rot);
-
-	// Transform unit vectors by rotation matrix
-	m_Forward = glm::vec3(rotMat * unit_z);
-	m_Right = glm::vec3(rotMat * unit_x);
-	m_Up = glm::cross(m_Forward, m_Right);
 
 	// Calculate view matrix
 	CalculateViewMatrix();
@@ -69,10 +52,11 @@ void real::Camera::Init(const GameContext& context)
 
 void real::Camera::CalculateViewMatrix()
 {
-	const auto worldPos = GetOwner()->GetTransform()->GetWorldPosition();
+	const auto transform = GetOwner()->GetTransform();
+	const auto worldPos = transform->GetWorldPosition();
 
-	const auto target = worldPos - m_Forward;
-	m_View = glm::lookAt(worldPos, target, m_Up);
+	const auto target = worldPos - transform->GetForward();
+	m_View = glm::lookAt(worldPos, target, transform->GetUp());
 	m_ViewInverse = glm::inverse(m_View);
 }
 
@@ -81,49 +65,4 @@ void real::Camera::CalculateProjectionMatrix(const GameContext& /*settings*/)
 	const auto [width, height] = Renderer::GetInstance().GetSwapChain()->GetExtent();
 	const float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
 	m_Projection = glm::perspective(glm::radians(m_FOV), aspectRatio, m_NearPlane, m_FarPlane);
-}
-
-void real::Camera::HandleKeyboardInput(float dt)
-{
-	auto& input = CameraInputManager::GetInstance();
-
-	glm::vec3 move{ 0,0,0 };
-	move.y = -(input.IsKeyboardKey(InputState::down, SDL_SCANCODE_W) ? 1.0f : 0.0f);
-	if ((int)move.y == 0)
-		move.y = input.IsKeyboardKey(InputState::down, SDL_SCANCODE_S) ? 1.0f : 0.0f;
-
-	move.x = input.IsKeyboardKey(InputState::down, SDL_SCANCODE_D) ? 1.0f : 0.0f;
-	if ((int)move.x == 0)
-		move.x = -(input.IsKeyboardKey(InputState::down, SDL_SCANCODE_A) ? 1.0f : 0.0f);
-
-	move.z = input.IsKeyboardKey(InputState::down, SDL_SCANCODE_SPACE) ? 1.0f : 0.0f;
-	if ((int)move.z == 0)
-		move.z = -(input.IsKeyboardKey(InputState::down, SDL_SCANCODE_LSHIFT) ? 1.0f : 0.0f);
-
-	float speed = m_MoveSpeed;
-	if (input.IsKeyboardKey(InputState::down, SDL_SCANCODE_Q))
-		speed *= m_SpeedMultiplier;
-
-	if (move == glm::vec3{ 0,0,0 })
-		return;
-
-	auto worldPos = GetOwner()->GetTransform()->GetWorldPosition();
-	worldPos += m_Forward * move.y * speed * dt;
-	worldPos += glm::vec3(unit_y) * move.z * speed * dt;
-	worldPos += m_Right * move.x * speed * dt;
-	GetOwner()->GetTransform()->SetWorldPosition(worldPos);
-}
-
-void real::Camera::HandleMouseInput(float dt)
-{
-	glm::vec2 look{ 0,0 };
-	if (CameraInputManager::GetInstance().IsMouseButton(InputState::down, MouseButton::left))
-	{
-		const auto& v = CameraInputManager::GetInstance().GetMouseMovement();
-		look.x = static_cast<float>(v.x);
-		look.y = static_cast<float>(v.y);
-	}
-
-	m_Yaw += look.x * m_RotationSpeed * dt;
-	m_Pitch += look.y * m_RotationSpeed * dt;
 }
--- Real3D/Misc/Camera.h
+++ Real3D/Misc/Camera.h
@@ -27,13 +27,6 @@ namespace real
 		const glm::mat4& GetViewProjectionInverse() const { return m_ViewProjectionInverse; }
 
 	private:
-		glm::vec3 m_Up{ 0,1,0 };
-		glm::vec3 m_Forward{ 0,0,1 };
-		glm::vec3 m_Right{ 1,0,0 };
-		//glm::vec3 m_Rotation{ 0,0,0 };
-
-		static inline constexpr glm::vec4 unit_x{ 1,0,0,0 }, unit_y{ 0,1,0,0 }, unit_z{ 0,0,1,0 };
-
 		glm::mat4 m_View{};
 		glm::mat4 m_Projection{};
 		glm::mat4 m_ViewInverse{};
@@ -41,16 +34,12 @@ namespace real
 		glm::mat4 m_ViewProjectionInverse{};
 
 		float m_FarPlane{}, m_NearPlane{}, m_FOV{};
-		float m_Pitch{}, m_Yaw{};
 		float m_MoveSpeed{ 10.f }, m_SpeedMultiplier{ 2.5f }, m_RotationSpeed{ 600.f };
 
 		void Init(const GameContext& context);
 
 		void CalculateViewMatrix();
 		void CalculateProjectionMatrix(const GameContext& settings);
-
-		void HandleKeyboardInput(float dt);
-		void HandleMouseInput(float dt);
 	};
 }
 
