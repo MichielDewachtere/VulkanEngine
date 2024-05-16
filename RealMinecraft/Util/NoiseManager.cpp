f0bf02901987d25f991149725db9bfa1e4c8e4ff
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-16 20:32:12 +0200

    Added noise

---
+++ RealMinecraft/Util/NoiseManager.cpp
@@ -0,0 +1,28 @@
+﻿#include "NoiseManager.h"
+
+#include "SimplexNoise.h"
+
+NoiseManager::~NoiseManager() = default;
+
+void NoiseManager::Initialize(uint32_t seed)
+{
+	m_Seed = seed;
+	m_pTerrainNoise = std::make_unique<SimplexNoise>(0.25f, 12.f);
+	m_pCaveNoise = std::make_unique<SimplexNoise>(0.1f);
+	m_pSpaghetCaveNoise = std::make_unique<SimplexNoise>(0.4f);
+}
+
+float NoiseManager::GetTerrainNoiseValue(double x, double y, double gridSize) const
+{
+	return m_pTerrainNoise->fractal(4, x / gridSize, y / gridSize);
+}
+
+float NoiseManager::GetCaveNoiseValue(double x, double y, double z, double gridSize) const
+{
+	return m_pCaveNoise->fractal(1, x / gridSize, y / gridSize, z / gridSize);
+}
+
+float NoiseManager::GetSpaghetCaveNoiseValue(double x, double y, double z, double gridSize) const
+{
+	return m_pSpaghetCaveNoise->fractal(1, x / gridSize, y / gridSize, z / gridSize);
+}
---
+++ RealMinecraft/Util/NoiseManager.h
@@ -0,0 +1,38 @@
+﻿#ifndef NOISEMANAGER_H
+#define NOISEMANAGER_H
+
+#include <memory>
+#include <cstdint>
+
+#include <real_core/Singleton.h>
+
+#include "Util/SimplexNoise.h"
+
+class NoiseManager final : public real::Singleton<NoiseManager>
+{
+public:
+	virtual ~NoiseManager() override;
+
+	NoiseManager(const NoiseManager& other) = delete;
+	NoiseManager& operator=(const NoiseManager& rhs) = delete;
+	NoiseManager(NoiseManager&& other) = delete;
+	NoiseManager& operator=(NoiseManager&& rhs) = delete;
+
+	void Initialize(uint32_t seed);
+
+	float GetTerrainNoiseValue(double x, double y, double gridSize) const;
+	float GetCaveNoiseValue(double x, double y, double z, double gridSize) const;
+	float GetSpaghetCaveNoiseValue(double x, double y, double z, double gridSize) const;
+
+private:
+	friend class Singleton<NoiseManager>;
+	explicit NoiseManager() = default;
+
+	uint32_t m_Seed{ 0 };
+
+	std::unique_ptr<SimplexNoise> m_pTerrainNoise{ nullptr };
+	std::unique_ptr<SimplexNoise> m_pCaveNoise{ nullptr };
+	std::unique_ptr<SimplexNoise> m_pSpaghetCaveNoise{ nullptr };
+};
+
+#endif // NOISEMANAGER_H
