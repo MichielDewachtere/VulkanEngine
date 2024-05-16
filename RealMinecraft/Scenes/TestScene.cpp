6d9fc1138690b896dfc17b61a33bcfbedd4d577a
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-16 20:32:27 +0200

    Update TestScene.cpp

--- RealMinecraft/Scenes/TestScene.cpp
+++ RealMinecraft/Scenes/TestScene.cpp
@@ -10,8 +10,10 @@
 
 #include "Commands/MoveCommand.h"
 #include "Commands/RotateCommand.h"
-#include "Components/Chunk.h"
+#include "Components/Player.h"
+#include "Components/World.h"
 #include "Util/BlockParser.h"
+#include "Util/NoiseManager.h"
 
 TestScene::TestScene(std::string name, std::string inputMap)
 	: Scene(std::move(name), std::move(inputMap))
@@ -21,57 +23,22 @@ TestScene::TestScene(std::string name, std::string inputMap)
 void TestScene::Load()
 {
 	using namespace real;
-
-	//const auto& materialManager = MaterialManager::GetInstance();
 	const auto context = RealEngine::GetGameContext();
 
-	//std::vector<PosTexNorm> vertices;
-	//std::vector<uint32_t> indices;
-
-	//auto v = BlockParser::GetInstance().GetFaceData(EDirection::north, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 0, 1, 2, 2, 3, 0 });
-
-	//v = BlockParser::GetInstance().GetFaceData(EDirection::east, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 4, 5, 6, 6, 7, 4 });
-
-	//v = BlockParser::GetInstance().GetFaceData(EDirection::south, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 8, 9, 10, 10, 11, 8 });
-
-	//v = BlockParser::GetInstance().GetFaceData(EDirection::west, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 12, 13, 14, 14, 15, 12 });
-
-	//v = BlockParser::GetInstance().GetFaceData(EDirection::up, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 16, 17, 18, 18, 19, 16 });
-
-	//v = BlockParser::GetInstance().GetFaceData(EDirection::down, EBlock::debug);
-	//vertices.insert(vertices.end(), v.begin(), v.end());
-	//indices.insert(indices.end(), { 20, 21, 22, 22, 23, 20 });
+	NoiseManager::GetInstance().Initialize(0);
 
 	{
-		auto& chunk = CreateGameObject();
-		chunk.GetTransform()->SetLocalPosition({ 0,0,0 });
+		auto& world = CreateGameObject();
+		world.GetTransform()->SetLocalPosition({ 0,0,0 });
 
-		chunk.AddComponent<Chunk>();
+		world.AddComponent<World>();
 	}
-	//{
-	//	auto& chunk = CreateGameObject();
-	//	chunk.GetTransform()->SetLocalPosition({ 8,0,0 });
-
-	//	chunk.AddComponent<Chunk>();
-	//	//mesh->AddVertices(vertices);
-	//	//mesh->AddIndices(indices);
-	//	//mesh->Init(context);
-	//}
 
 	TransformInfo info;
 	info.position = { 8,65,8 };
-	auto& camera = CreateGameObject(info);
+	auto& camera = CreateGameObject(info, "player");
 	{
+		camera.AddComponent<Player>();
 		const auto comp = camera.AddComponent<Camera>();
 		CameraManager::GetInstance().AddCamera(comp, true);
 	}
