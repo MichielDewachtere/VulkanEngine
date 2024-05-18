29636903078059fa9c20e764fe1bb196aded87cb
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-18 14:15:07 +0200

    Update Material.h

--- Real3D/Material/Material.h
+++ Real3D/Material/Material.h
@@ -55,20 +55,6 @@ namespace real
 
 			vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pPipeline->GetPipeline());
 
-			VkViewport viewport{};
-			viewport.x = 0.0f;
-			viewport.y = 0.0f;
-			viewport.width = (float)extent.width;
-			viewport.height = (float)extent.height;
-			viewport.minDepth = 0.0f;
-			viewport.maxDepth = 1.0f;
-			vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
-
-			VkRect2D scissor{};
-			scissor.offset = { 0, 0 };
-			scissor.extent = extent;
-			vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
-
 			// DrawScene();
 			for (const auto& m : m_pMeshes)
 			{
