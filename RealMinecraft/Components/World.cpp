777458c3b0930d5be9b904d44ff292bc5d59772a
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-27 18:04:59 +0200

    Updated world component

--- RealMinecraft/Components/World.cpp
+++ RealMinecraft/Components/World.cpp
@@ -7,6 +7,7 @@
 #include "Util/Macros.h"
 #include "Components/Chunk.h"
 #include "real_core/GameTime.h"
+#include "real_core/SceneManager.h"
 
 World::World(real::GameObject* pOwner)
 	: Component(pOwner)
@@ -22,7 +23,12 @@ void World::Start()
 		{
 			const auto chunkPos = glm::vec3(x * CHUNK_SIZE, 0, z * CHUNK_SIZE);
 			auto& go = GetOwner()->CreateGameObject({ chunkPos });
-			m_pChunks[glm::vec2{ chunkPos.x, chunkPos.z }] = go.AddComponent<Chunk>();
+
+			auto iChunkPos = glm::ivec2{ chunkPos.x, chunkPos.z };
+			if (m_BlocksForFutureChunks.contains(iChunkPos))
+				m_pChunks[iChunkPos] = go.AddComponent<Chunk>(m_BlocksForFutureChunks.at(iChunkPos));
+			else
+				m_pChunks[iChunkPos] = go.AddComponent<Chunk>();
 		}
 	}
 #else
@@ -30,26 +36,72 @@ void World::Start()
 	auto& go = GetOwner()->CreateGameObject({ chunkPos });
 	m_pChunks[glm::vec2{ chunkPos.x, chunkPos.z }] = go.AddComponent<Chunk>();
 #endif // SINGLE_CHUNK
-	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerPosChanged.AddObserver(this);
+	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerMovedChunk.AddObserver(this);
+	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerMovedBlock.AddObserver(this);
+
+	m_pChunks.at(m_CurrentChunkPos)->SetAsCenter(true);
 }
 
 void World::Update()
 {
+	if (m_ChunksToAdd.empty())
+		return;
+
+	const auto [chunkPos, adjacentChunk, direction] = m_ChunksToAdd.front();
+	m_ChunksToAdd.pop_front();
+
+	const auto worldPos = glm::vec3{ chunkPos.x,0,chunkPos.y };
+
+	auto& go = GetOwner()->CreateGameObject({ worldPos });
+
+	if (m_BlocksForFutureChunks.contains(chunkPos))
+		m_pChunks[chunkPos] = go.AddComponent<Chunk>(m_BlocksForFutureChunks.at(chunkPos));
+	else
+		m_pChunks[chunkPos] = go.AddComponent<Chunk>();
+
+	m_pChunks.at(adjacentChunk)->UpdateChunkBoarder(m_pChunks.at(chunkPos), direction);
+
+	// Also update the previously added chunk
+	const auto otherDir = glm::ivec2{ direction.y * -1, direction.x * -1 };
+	const auto otherChunkPos = chunkPos - otherDir;
+	if (m_pChunks.contains(otherChunkPos))
+		m_pChunks.at(otherChunkPos)->UpdateChunkBoarder(m_pChunks.at(chunkPos), otherDir);
+
+	m_IsDirty = true;
+}
+
+void World::LateUpdate()
+{
+	if (m_FirstFrame)
+	{
+		m_FirstFrame = false;
+		return;
+	}
+
+	if (m_IsDirty == false)
+		return;
+
+	SortChunks(m_CurrentChunkPos);
+
+	m_IsDirty = false;
 }
 
 void World::Kill()
 {
-	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerPosChanged.RemoveObserver(this);
+	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerMovedChunk.RemoveObserver(this);
+	real::SceneManager::GetInstance().GetActiveScene().GetGameObject(1)->GetComponent<Player>()->playerMovedBlock.RemoveObserver(this);
 }
 
-void World::HandleEvent(const glm::ivec2& chunkPos)
+void World::HandleEvent(Player::Events, const glm::ivec2& chunkPos)
 {
 #ifdef SINGLE_CHUNK
 	return;
 #endif // SINGLE_CHUNK
 
-	const auto id = real::GameTime::GetInstance().StartTimer();
-	std::cout << "In chunk: [" << chunkPos.x << ';' << chunkPos.y << "]\n";
+	m_pChunks.at(m_CurrentChunkPos)->SetAsCenter(false);
+	m_CurrentChunkPos = chunkPos * CHUNK_SIZE;
+	m_pChunks.at(m_CurrentChunkPos)->SetAsCenter(true);
+	m_IsDirty = true;
 
 	const auto maxX = (chunkPos.x + render_distance) * CHUNK_SIZE;
 	const auto minX = (chunkPos.x - render_distance) * CHUNK_SIZE;
@@ -62,13 +114,13 @@ void World::HandleEvent(const glm::ivec2& chunkPos)
 	std::map<int, std::vector<glm::ivec2>> dirtyChunks;
 
 	// Register chunks to add/delete/update
-	for (const auto& pos : m_pChunks | std::views::keys) 
+	for (const auto& pos : m_pChunks | std::views::keys)
 	{
 		glm::ivec2 newPos = pos;
 		glm::ivec2 chunkToUpdate = pos;
 		bool toAdd = false;
 
-		if (pos.x > maxX || pos.x < minX) 
+		if (pos.x > maxX || pos.x < minX)
 		{
 			newPos.x += (pos.x > maxX ? -chunkShift : chunkShift);
 			chunkToUpdate.x = newPos.x + (pos.x < maxX ? -CHUNK_SIZE : CHUNK_SIZE);
@@ -78,7 +130,7 @@ void World::HandleEvent(const glm::ivec2& chunkPos)
 			//dirtyChunks.push_back({ pos,remove });
 		}
 
-		if (pos.y > maxY || pos.y < minY) 
+		if (pos.y > maxY || pos.y < minY)
 		{
 			newPos.y += (pos.y > maxY ? -chunkShift : chunkShift);
 			chunkToUpdate.y = newPos.y + (pos.y < maxY ? -CHUNK_SIZE : CHUNK_SIZE);
@@ -99,6 +151,9 @@ void World::HandleEvent(const glm::ivec2& chunkPos)
 	}
 
 	glm::ivec2 direction;
+	if (dirtyChunks[add].empty())
+		return;
+
 	if (dirtyChunks[add].front().x < dirtyChunks[update].front().x)
 		direction = { -CHUNK_SIZE,0 };
 	else if (dirtyChunks[add].front().x > dirtyChunks[update].front().x)
@@ -114,22 +169,16 @@ void World::HandleEvent(const glm::ivec2& chunkPos)
 		{
 		case add:
 		{
-			std::ranges::for_each(chunks, [this](const glm::ivec2& pos)
-				{
-					auto worldPos = glm::vec3{ pos.x,0,pos.y };
-					std::cout << "added chunk at position " << worldPos.x << ',' << worldPos.y << ',' << worldPos.z << '\n';
-
-					auto& go = GetOwner()->CreateGameObject({ worldPos });
-					m_pChunks[glm::vec2{ worldPos.x, worldPos.z }] = go.AddComponent<Chunk>();
-				});
+			//std::ranges::for_each(chunks, [this](const glm::ivec2& pos)
+			//	{
+			//		m_ChunksToAdd[pos] = {};
+			//	});
 			break;
 		}
 		case remove:
 		{
 			std::ranges::for_each(chunks, [this](const glm::ivec2& pos)
 				{
-					std::cout << "Removing chunk at position " << pos.x << ',' << 0 << ',' << pos.y << '\n';
-
 					m_pChunks[pos]->GetOwner()->Destroy();
 					m_pChunks.erase(glm::vec2{ pos.x, pos.y });
 				});
@@ -140,23 +189,66 @@ void World::HandleEvent(const glm::ivec2& chunkPos)
 			std::ranges::for_each(chunks, [this, direction](const glm::ivec2& pos)
 				{
 					const auto adjacentChunk = pos + direction;
-					std::cout << "Updating chunk at position " << pos.x << ',' << 0 << ',' << pos.y << '\n';
-					std::cout << "Adjacent chunk has position " << adjacentChunk.x << ',' << 0 << ',' << adjacentChunk.y << '\n';
-					m_pChunks[pos]->UpdateChunkBoarder(m_pChunks[adjacentChunk], direction);
+					m_ChunksToAdd.emplace_back(adjacentChunk, pos, direction);
 				});
 			break;
 		}
 		}
 	}
+}
 
-	auto time = real::GameTime::GetInstance().EndTimer(id);
-	std::cout << "time to update world " << time << " milliseconds\n";
+void World::HandleEvent(Player::Events, const glm::ivec3& playerPos)
+{
+	m_pChunks.at(m_CurrentChunkPos)->SortBlocks(playerPos);
 }
 
 Chunk* World::GetChunkAt(const glm::ivec2& chunkPos) const
 {
-	if (m_pChunks.contains(chunkPos) == false)
-		return nullptr;
+	//const auto it = std::ranges::find_if(m_pChunks, [chunkPos](const std::pair<glm::ivec2, Chunk*>& pair)
+	//	{
+	//		return pair.first == chunkPos;
+	//	});
 
-	return m_pChunks.at(chunkPos);
+	//if (it != m_pChunks.end())
+	//{
+	//	return it->second;
+	//}
+
+	//return nullptr;
+
+	if (m_pChunks.contains(chunkPos))
+		return m_pChunks.at(chunkPos);
+
+	return nullptr;
+}
+
+void World::AddBlocksForFutureChunks(const glm::ivec2& chunkPos, const std::vector<std::pair<glm::ivec3, EBlock>>&
+                                     blocks)
+{
+	m_BlocksForFutureChunks[chunkPos].insert(m_BlocksForFutureChunks[chunkPos].end(), blocks.begin(), blocks.end());
+}
+
+void World::SortChunks(const glm::ivec2& center)
+{
+	const auto pChild = GetOwner()->GetScene().GetGameObject(1);
+	const auto pos = pChild->GetTransform()->GetWorldPosition();
+	const glm::vec2 vec2 = { pos.x,pos.z };
+
+	std::vector<std::pair<glm::ivec2, Chunk*>> vec(m_pChunks.begin(), m_pChunks.end());
+
+	std::ranges::sort(vec, [&center](const auto& a, const auto& b)
+		{
+			auto distanceSquared = [](const glm::ivec2& p1, const glm::ivec2& p2)
+				{
+					const glm::ivec2 diff = p1 - p2;
+					return diff.x * diff.x + diff.y * diff.y;
+				};
+
+			return distanceSquared(a.first, center) > distanceSquared(b.first, center);
+		});
+
+	for (const auto& chunk : vec | std::views::values)
+	{
+		GetOwner()->MoveChildBack(chunk->GetOwner());
+	}
 }
--- RealMinecraft/Components/World.h
+++ RealMinecraft/Components/World.h
@@ -1,6 +1,7 @@
 #ifndef	WORLD_H
 #define WORLD_H
 
+#include <deque>
 #include <map>
 #include <vector>
 
@@ -9,20 +10,29 @@
 #include <real_core/Component.h>
 #include <real_core/Observer.h>
 
+#include "Player.h"
+
+enum class EBlock;
 class Chunk;
 
-// Custom comparator for glm::vec3
-struct Vec2Comparator {
-	bool operator()(const glm::vec2& lhs, const glm::vec2& rhs) const {
-		if (lhs.x < rhs.x) return true;
-		if (lhs.x > rhs.x) return false;
-		return (lhs.y < rhs.y);
+template<glm::length_t L, typename T, glm::qualifier Q = glm::defaultp>
+struct VecComparator
+{
+	bool operator()(const glm::vec<L, T, Q>& lhs, const glm::vec<L, T, Q>& rhs) const
+	{
+		for (int i = 0; i < L; ++i) 
+		{
+			if (lhs[i] < rhs[i]) return true;
+			if (lhs[i] > rhs[i]) return false;
+		}
+		return false;
 	}
 };
 
 class World final
 	: public real::Component
-	, public real::Observer<const glm::ivec2&>
+	, public real::Observer<Player::Events, const glm::ivec2&>
+	, public real::Observer<Player::Events, const glm::ivec3&>
 {
 public:
 	explicit World(real::GameObject* pOwner);
@@ -35,17 +45,27 @@ public:
 
 	virtual void Start() override;
 	virtual void Update() override;
+	virtual void LateUpdate() override;
 	virtual void Kill() override;
 
-	void HandleEvent(const glm::ivec2&) override;
+	void HandleEvent(Player::Events, const glm::ivec2&) override;
+	void HandleEvent(Player::Events, const glm::ivec3&) override;
 	void OnSubjectDestroy() override {}
 
 	Chunk* GetChunkAt(const glm::ivec2& chunkPos) const;
+	void AddBlocksForFutureChunks(const glm::ivec2& chunkPos, const std::vector<std::pair<glm::ivec3, EBlock>>& blocks);
 
 private:
-	static constexpr inline int render_distance{ 4 };
+	static constexpr inline int render_distance{ 12 };
+	bool m_IsDirty{ true }, m_FirstFrame{ true };
+	glm::ivec2 m_CurrentChunkPos{ 0,0 };
 
-	std::map<glm::ivec2, Chunk*, Vec2Comparator> m_pChunks{};
+	std::map<glm::ivec2, Chunk*, VecComparator<2, int>> m_pChunks{};
+	std::map<glm::ivec2, std::vector<std::pair<glm::ivec3, EBlock>>, VecComparator<2, int>> m_BlocksForFutureChunks{};
+
+	std::deque<std::tuple<glm::ivec2, glm::ivec2, glm::ivec2>> m_ChunksToAdd{};
+
+	void SortChunks(const glm::ivec2& center);
 };
 
 #endif // WORLD_H
