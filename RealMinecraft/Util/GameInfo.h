97f34a6efb7747e1cc72385af956276774ed15ce
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-27 18:06:47 +0200

    Keeps track of material ids

---
+++ RealMinecraft/Util/GameInfo.h
@@ -0,0 +1,16 @@
+#ifndef GAMEINFO_H
+#define GAMEINFO_H
+
+#include <cstdint>
+
+struct Materials
+{
+	static inline uint8_t diffuseMaterial;
+	static inline uint8_t waterMaterial;
+	static inline uint8_t transparentMaterial;
+	static inline uint8_t transpriteMaterial;
+	static inline uint8_t outlineMaterial;
+	static inline uint8_t guiMaterial;
+};
+
+#endif // GAMEINFO_H
